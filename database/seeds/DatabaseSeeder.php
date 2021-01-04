7bd714698cd860a5c017b87a3765b192bda6db17
navinkodag666 <navin@mybytecode.com>
2021-01-05 01:04:47 +0530

    fix_for_migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,22 +1,28 @@
 <?php
+
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
+
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
     public function run()
-{
-    $this->call([
-        CustomerUsersSeeder::class,
-        CustomerBusinessSeeder::class,
-        CustomerBusinessUsersMappingSeeder::class,
-        PartnerBusinessCustomerBusinessCreditTransactionsSeeder::class,
-        PartnerBusinessCustomerBusinessMappingSeeder::class,
-        PartnerBusinessCustomerBusinessDiscountsSeeder::class,
-        PartnerBusinessCustomeerBusinessPaymentsSeeder::class,
-        PartnerBusinessCustomerBusinessTransactionsPaymentDiscountsSeeder::class,
-        PartnerBusinessSeeder::class,
-        PartnerBusinessUsersMappingSeeder::class,
-        PartnerUserSeeder::class,
-    ]);
-}
+    {
+        $this->call([
+            CustomerUsersSeeder::class,
+            PartnerUserSeeder::class,
+            PartnerBusinessSeeder::class,
+            CustomerBusinessSeeder::class,
+            CustomerBusinessUsersMappingSeeder::class,
+            PartnerBusinessCustomerBusinessMappingSeeder::class,
+            PartnerBusinessCustomerBusinessCreditTransactionsSeeder::class,
+            PartnerBusinessCustomerBusinessDiscountsSeeder::class,
+            PartnerBusinessCustomerBusinessPaymentsSeeder::class,
+            PartnerBusinessCustomerBusinessTransactionsPaymentsDiscountsSeeder::class,
+            PartnerBusinessUsersMappingSeeder::class,
+        ]);
+    }
 }
--- database/seeds/PartnerBusinessCustomerBusinessDiscountsSeeder.php
+++ database/seeds/PartnerBusinessCustomerBusinessDiscountsSeeder.php
@@ -18,11 +18,11 @@ class PartnerBusinessCustomerBusinessDiscountsSeeder extends Seeder
         for ($i = 0; $i <= 100; $i++) :
             DB::table('p_business_c_business_discounts')
                 ->insert([
-                    'p_c_mapping_id' => $faker->bankAccountNumber,
-                    'p_c_transaction_id' => $faker->bankAccountNumber,
+                    'p_c_mapping_id' => rand(1,99),
+                    'p_c_transaction_id' => rand(1,99),
                     'added_by' => rand(1, 99),
                     'edit_updated_on' => $faker->dateTime($max = 'now', $timezone = null),
-                    'edit_updated_by' => rand(1, 99),
+                    'edit_updated_by' => $faker->dateTime($max = 'now', $timezone = null),
                     'discount_date' => $faker->dateTime($max = 'now', $timezone = null),
                     'discount_amount' => rand(200, 400),
                     'discount_rate' => rand(1, 66),
